14438da717ea8ef886e4b299d000b136abac7a71
salmoncode <salmon2073@gmail.com>
2026-02-11 17:23:22 +0900

    feat: implement ClientPage component for hearing section

--- app/hearing/[category]/[section]/page.tsx
+++ app/hearing/[category]/[section]/page.tsx
@@ -1,36 +1,21 @@
-"use client";
-import { Label } from "@/components/ui/label"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { Textarea } from "@/components/ui/textarea"
-import { useState } from 'react';
+import { Categories } from '../../config';
+import ClientPage from './client-page';
+
+export function generateStaticParams() {
+    const params: { category: string; section: string }[] = [];
+
+    Object.keys(Categories).forEach(category => {
+        Categories[category].sections.forEach(section => {
+            params.push({
+                category,
+                section: section.title
+            });
+        });
+    });
+
+    return params;
+}
 
 export default function Page() {
-    const [textLength, setTextLength] = useState(0);
-    return (<>
-        <div className="mt-8 mb-12">
-            <h2 className="main-content-label mb-4 text-l font-bold">
-                状況
-            </h2>
-            <RadioGroup defaultValue="comfortable" className="w-fit">
-                <div className="flex items-center gap-3">
-                    <RadioGroupItem value="default" id="r1" />
-                    <Label htmlFor="r1">新しくWebサイトを作りたい</Label>
-                </div>
-                <div className="flex items-center gap-3">
-                    <RadioGroupItem value="comfortable" id="r2" />
-                    <Label htmlFor="r2">今あるWebサイトをリニューアルしたい</Label>
-                </div>
-            </RadioGroup>
-        </div>
-        <div className="mb-24">
-            <h2 className="main-content-label mb-4 text-l font-bold">
-                要約
-            </h2>
-            <Textarea id="textarea-message" className="min-h-[240px]" placeholder="ヒアリングシート内容を反映"
-                onChange={(e) => setTextLength(e.target.value.length)} />
-            <div className="text-right">
-                {textLength + "/400"}
-            </div>
-        </div>
-    </>);
+    return <ClientPage />;
 }
